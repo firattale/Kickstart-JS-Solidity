470a43bd34b6df47c33727e3d253af95cb68d5fd
Firat Tale <firat.tale@auto1.com>
2022-01-11 10:28:03 +0100

    prettier contact

--- ethereum/contracts/Campaign.sol
+++ ethereum/contracts/Campaign.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.11;
 contract CampaignFactory {
     address[] public deployedCampaigns;
 
-    function createCampaign(uint minimum) public {
+    function createCampaign(uint256 minimum) public {
         Campaign newCampaign = new Campaign(minimum, msg.sender);
         deployedCampaigns.push(address(newCampaign));
     }
@@ -17,18 +17,18 @@ contract CampaignFactory {
 contract Campaign {
     struct Request {
         string description;
-        uint value;
+        uint256 value;
         address payable recipient;
         bool complete;
-        uint approvalCount;
+        uint256 approvalCount;
         mapping(address => bool) approvals;
     }
 
-    mapping (uint => Request) requests;
+    mapping(uint256 => Request) requests;
     address public manager;
-    uint public minimumContribution;
+    uint256 public minimumContribution;
     mapping(address => bool) public approvers;
-    uint public approversCount;
+    uint256 public approversCount;
 
     mapping(address => bool) requestpprovals;
 
@@ -37,48 +37,63 @@ contract Campaign {
         _;
     }
 
-    constructor (uint minimum, address creator) {
+    constructor(uint256 minimum, address creator) {
         manager = creator;
         minimumContribution = minimum;
     }
 
     function contribute() public payable {
-        require(msg.value >= minimumContribution, "Amount sent is less than the minimum contribution limit.");
+        require(
+            msg.value >= minimumContribution,
+            "Amount sent is less than the minimum contribution limit."
+        );
 
         approvers[msg.sender] = true;
         approversCount++;
     }
-    uint numRequests;
 
-    function createRequest(string memory description, uint value, address payable recipient) public onlyOwner {
-        require(value <= address(this).balance, "Not enough money to request that amount");
+    uint256 numRequests;
+
+    function createRequest(
+        string memory description,
+        uint256 value,
+        address payable recipient
+    ) public onlyOwner {
+        require(
+            value <= address(this).balance,
+            "Not enough money to request that amount"
+        );
         Request storage r = requests[numRequests++];
 
-     r.description = description;
+        r.description = description;
         r.value = value;
         r.recipient = recipient;
         r.complete = false;
         r.approvalCount = 0;
-
-        // requests.push(r);
     }
 
-    function approveRequest(uint index) public {
+    function approveRequest(uint256 index) public {
         Request storage request = requests[index];
 
         require(approvers[msg.sender], "You are not an approver.");
-        require(request.approvals[msg.sender] == false, "You have already voted on this request.");
+        require(
+            request.approvals[msg.sender] == false,
+            "You have already voted on this request."
+        );
         require(request.complete == false, "Request was already finalized.");
 
         request.approvals[msg.sender] = true;
         request.approvalCount++;
     }
 
-    function finalizeRequest(uint index) public onlyOwner {
+    function finalizeRequest(uint256 index) public onlyOwner {
         Request storage request = requests[index];
 
         require(request.complete == false, "Request was already finalized.");
-        require(request.approvalCount > (approversCount / 2), "Not enough approvals.");
+        require(
+            request.approvalCount > (approversCount / 2),
+            "Not enough approvals."
+        );
 
         request.recipient.transfer(request.value);
         request.complete = true;
